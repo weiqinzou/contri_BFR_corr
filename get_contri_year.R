158043fc5a53b4d5e70d570cfce682d3e24c3254
zouxiaoyu <weiqinzou@qq.com>
2015-10-14 19:32:09 +0800

    fixed some bugs about cal preContri

--- get_contri_year.R
+++ get_contri_year.R
@@ -9,7 +9,6 @@ ori_rpt<-argv[1]
 ori_fix<-argv[2]
 ori_cmt<-argv[3]
 rpt_thr<-argv[4]
-contri<-argv[5]
 
 
 #filter out the non-closed bugs, we only use closed bugs to conduct the experiment
@@ -29,10 +28,15 @@ get_rpt_closed<-function(ori_rpt,rpt_thr){
 }
 
 preContri<-function(contri_eachyear_d,contri_fout){
-#contri_eachyear_d is a dataframe which contains such items:
+#contri_eachyear_d is a dataframe which contains such items:bug_id,who,when
+    contri_eachyear_d<-ddply(contri_eachyear_d,.(who,year=substr(when,1,4)),
+                             summarize,cnt=length(bug_id))
     names(contri_eachyear_d)<-c("who","when","cnt")
     contributors<-unique(contri_eachyear_d$who)
-#!!!!need to clear the contri_fout and write the head into the contri_fout before for loop
+#clear the contri_fout and write the head into the contri_fout before for loop
+    head<-c("who","when","cnt","cntPre\n")
+    cat(head,file=contri_fout,sep=",",append=F)
+
     for(c in contributors){
         contri<-contri_eachyear_d[contri_eachyear_d$who==c,]
         contri<-contri[order(contri$who,contri$when),]
@@ -42,38 +46,39 @@ preContri<-function(contri_eachyear_d,contri_fout){
     }
 }
 
-#get each contributor's contribution by year. without divided by products.
-#note that the rpt file is all closed bugs containing file
-total_contri<-function(rpt_fin,fix_fin,cmt_fin,rpt_thr,contri_fout){
-    r<-get_rpt_closed(rpt_fin,rpt_thr)
-    ryear<-ddply(r,.(reporter,year=substr(creation_ts,1,4)),summarize,rptCnt=length(bug_id))
-    names(ryear)<-c("who","when","rptCnt")    
-    rm(r)
-    gc()
-    preContri(ryear,paste(rpt_fin,'preCnt',sep=''))
-#    ryear<-ryear[order(ryear$who,ryear$when),]
-    #ryear<-ddply(ryear,.(who),transform,rptCntPre=cumsum(rptCnt)-rptCnt)
-    print(nrow(ryear))
-    print(head(ryear))
-    q()
-
-    f<-read.csv(fix_fin,header=T,sep=",")
-    fyear<-ddply(f,.(fixer,year=substr(fixTime,1,4)),summarize,fixCnt=length(bug_id))
-    names(fyear)<-c("who","when","fixCnt")
-#    fyear<-fyear[order(fyear$who,fyear$when),]
-    fyear<-ddply(fyear,.(who),transform,fixCntPre=cumsum(fixCnt)-fixCnt)
-
-    c<-read.csv(cmt_fin,header=T,sep=",")
-    cyear<-ddply(c,.(cmtor,year=substr(cmtTime,1,4)),summarize,cmtCnt=length(bug_id))
-    names(cyear)<-c("who","when","cmtCnt")
-#    cyear<-cyear[order(cyear$who,cyear$when),]
-    cyear<-ddply(cyear,.(who),transform,cmtCntPre=cumsum(cmtCnt)-cmtCnt)
-
-#merge rptCnt,fixCnt and cmtCnt for each contributor by year
-    rf<-merge(ryear,fyear,by=c("who","when"),all.x=T)
-    rfc<-merge(rf,cyear,by=c("who","when"),all.x=T)
-    write.table(rfc,file=contri,sep=",",col.names=T,row.names=F)
+fixcmtor_preContri<-function(fixcmt_d,activeRptor,contri_fout){
+    names(fixcmt_d)<-c("bug_id","who","when")
+#filter the fixer or cmtor who are not within the active reporters.
+    fixcmt_d<-fixcmt_d[fixcmt_d$who %in% activeRptor,]
+    preContri(fixcmt_d,contri_fout)
 }
 
-total_contri(ori_rpt,ori_fix,ori_cmt,rpt_thr,contri)
+get_rptor_BFR<-function(rpt_d,BFR_fout){
+#rpt_d is a dataframe which contains:bug_id,reporter,resolution,creation_ts
+    rd<-ddply(rpt_d,.(reporter,year=substr(creation_ts,1,4)),summarize,
+          BFR=length(resolution[tolower(resolution)=="fixed"])/length(resolution))
+    names(rd)<-c("who","when","BFR")
+    write.table(rd,BFR_fout,sep=",",col.names=T,row.names=F) 
+}
+#get each contributor's contribution by year. without divided by products.
+#note that the rpt file is all closed bugs containing file
+total_contri<-function(rpt_fin,fix_fin,cmt_fin,rpt_thr){
+    r<-get_rpt_closed(rpt_fin,rpt_thr)
+    r<-r[,c("bug_id","reporter","creation_ts","resolution")]
+    get_rptor_BFR(r,paste(rpt_fin,'BFR',sep='_'))
+
+    r<-r[,-4]
+    names(r)<-c("bug_id","who","when")
+    preContri(r,paste(rpt_fin,'preCnt',sep='_'))
+
+    p<-unique(r$who)
+    f<-read.csv(fix_fin,header=T,sep=",")
+    fixcmtor_preContri(f,p,paste(fix_fin,'preCnt',sep='_'))   
+    
+    c<-read.csv(cmt_fin,header=T,sep=",")
+    fixcmtor_preContri(c,p,paste(cmt_fin,'preCnt',sep='_'))   
+}
+
+
+total_contri(ori_rpt,ori_fix,ori_cmt,rpt_thr)
 
