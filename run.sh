772cf32f7a95bc5c4964883051922b35edda0a3d
zou <zou@debian.zou-denian>
2015-10-16 20:27:24 +0800

    add corr cal script running step to run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ Rscript --slave final_get_contri.R rptInfo_M fixInfo_M cmtInfo_M 10 M > run_M.lo
 #merge the rpt|fix|cmt contri
 Rscript --slave merge_contri_year.R rptInfo_E_BFR E_rpt E_fix E_cmt E
 Rscript --slave merge_contri_year.R rptInfo_M_BFR M_rpt M_fix M_cmt M
+
+#cal correlation between BFR and kinds of contributions(rpt|fix|cmt)
+Rscript --slave pre_cur_Cnt_BFR_corr.R E_BFR_rfc > allContri_BFR_corr_E.res
+Rscript --slave pre_cur_Cnt_BFR_corr.R M_BFR_rfc > allContri_BFR_corr_M.res
