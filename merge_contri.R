362d4559cfa9cc641d3af5e17c9ddb41383a97e3
zou <zou@debian.zou-denian>
2015-10-16 09:46:46 +0800

    refactor the merge_contri_year.R and run.sh

--- merge_contri.R
+++ merge_contri.R
@@ -1,33 +1,36 @@
 library('plyr',lib.loc="~/myOwnRPackage")
 
 #run example:
-#Rscript --slave merge_contri_year.R rptInfo_M_BFR rptInfo_M_preCnt fixInfo_M_preCnt cmtInfo_M_preCnt rfc_M_preCnt&
-
-#Rscript --slave merge_contri_year.R rptInfo_E_BFR rptInfo_E_preCnt fixInfo_E_preCnt cmtInfo_E_preCnt rfc_E_preCnt&
+#Rscript --slave merge_contri_year.R rptInfo_M_BFR M_rpt M_fix M_cmt M
+#Rscript --slave merge_contri_year.R rptInfo_E_BFR E_rpt E_fix E_cmt E
 
 argv<-commandArgs(TRUE)
-rpt_preCnt<-argv[1]
-fix_preCnt<-argv[2]
-cmt_preCnt<-argv[3]
-rpt_BFR<-argv[4]
-rfc_preCnt<-argv[5]
+rpt_BFR<-argv[1]
+rpt_preCnt<-argv[2]
+fix_preCnt<-argv[3]
+cmt_preCnt<-argv[4]
+proj_name<-argv[5]
 
-#input data format:rpt|fix|cmt_preCnt contains: who, when, cnt,cntPre
+#input data format:rpt|fix|cmt_preCnt contains: who,when,curCnt,preCnt
 #rptBFR contains:who,when,BFR
-merge_preCnt<-function(rPreCnt_fin,fPreCnt_fin,cPreCnt_fin,
-                       rptBFR_fin,rfc_fout){
-    r<-read.csv(rPreCnt_fin,header=T,sep=",")
-    names(r)<-c("who","when","rptCnt","rptCntPre")
-    f<-read.csv(fPreCnt_fin,header=T,sep=",")
-    names(f)<-c("who","when","fixCnt","fixCntPre")
-    c<-read.csv(cPreCnt_fin,header=T,sep=",")
-    names(c)<-c("who","when","cmtCnt","cmtCntPre")
+merge_contri<-function(rptBFR_fin,rPreCnt_fin,fPreCnt_fin,cPreCnt_fin,
+                       proj_name){
     b<-read.csv(rptBFR_fin,header=T,sep=",")
+    names(b)<-c("who","when","BFR")
+
+    r<-read.csv(rPreCnt_fin,header=T,sep=",")
+    names(r)<-c("who","when","rptCnt","preRptCnt")
+    f<-read.csv(fPreCnt_fin,header=T,sep=",")
+    names(f)<-c("who","when","fixCnt","preFixCnt")
+    c<-read.csv(cPreCnt_fin,header=T,sep=",")
+    names(c)<-c("who","when","cmtCnt","preCmtCnt")
+    
     #merge rpt|fix|cmt preCnt with BFR
     br<-merge(b,r,by=c("who","when"),all.x=T)
     brf<-merge(br,f,by=c("who","when"),all.x=T)
     brfc<-merge(brf,c,by=c("who","when"),all.x=T)
+    rfc_fout<-paste(proj_name,'BFR','rfc',sep='_')
     write.table(brfc,rfc_fout,sep=",",col.names=T,row.names=F)
 }
 
-merge_preCnt(rpt_preCnt,fix_preCnt,cmt_preCnt,rpt_BFR,rfc_preCnt)
+merge_contri(rpt_BFR,rpt_preCnt,fix_preCnt,cmt_preCnt,proj_name)
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
+#get the rpt|fix|cmt contri
 Rscript --slave final_get_contri.R rptInfo_E fixInfo_E cmtInfo_E 10 E > run_E.log 2 >&1&
 Rscript --slave final_get_contri.R rptInfo_M fixInfo_M cmtInfo_M 10 M > run_M.log 2>&1&
+
+#merge the rpt|fix|cmt contri
+Rscript --slave merge_contri_year.R rptInfo_E_BFR E_rpt E_fix E_cmt E
+Rscript --slave merge_contri_year.R rptInfo_M_BFR M_rpt M_fix M_cmt M
